4ec5316c33c3946cdea5436915917e06acb1cc14
Osmel <oprades4@gmail.com>
2025-09-07 21:58:01 -0400

    cors configurado

--- taskflow-back/index.js
+++ taskflow-back/index.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: true,
+  origin: "http://localhost:4000/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
